ef0a7dab902d6f483524a1e9eb11f0e050988e28
Roberto Filho <zferraz.rf@gmail.com>
2025-08-15 00:13:32 -0300

    feat: add configuration module for countryStateCity

---
+++ adapter/connector/countryStateCity/config.go
@@ -0,0 +1,36 @@
+package countryStateCity
+
+import (
+	"github.com/Netflix/go-env"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+)
+
+type Config struct {
+	CountryStateCityAPIKey *string `json:"country_state_city_api_key" env:"COUNTRY_STATE_CITY_API_KEY"`
+}
+
+var ConfigModule = fx.Module(
+	"country_state_api_config",
+	fx.Provide(NewConfig),
+)
+
+func NewConfig(l *zap.SugaredLogger) (*Config, error) {
+	var cfg Config
+	err := cfg.loadConfig()
+	if err != nil {
+		l.Error(err)
+		return nil, err
+	}
+
+	return &cfg, nil
+}
+
+func (c *Config) loadConfig() error {
+	_, err := env.UnmarshalFromEnviron(c)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
