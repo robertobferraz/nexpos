080dfa0d02bf833bbb2c73ef459ab7f543c97d65
Roberto Filho <zferraz.rf@gmail.com>
2025-08-15 20:57:08 -0300

    feat: implement configuration and initialization for openCep module

--- adapter/connector/openCep/config.go
+++ adapter/connector/openCep/config.go
@@ -1,1 +1,36 @@
 package openCep
+
+import (
+	"github.com/Netflix/go-env"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+)
+
+type Config struct {
+	OpenCepBaseURL *string `json:"open_cep_base_url" env:"OPEN_CEP_BASE_URL"`
+}
+
+var ConfigModule = fx.Module(
+	"open_cep_config",
+	fx.Provide(NewConfig),
+)
+
+func NewConfig(l *zap.SugaredLogger) (*Config, error) {
+	var cfg Config
+	err := cfg.loadConfig()
+	if err != nil {
+		l.Error(err)
+		return nil, err
+	}
+
+	return &cfg, nil
+}
+
+func (c *Config) loadConfig() error {
+	_, err := env.UnmarshalFromEnviron(c)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- adapter/connector/openCep/open_cep.go
+++ adapter/connector/openCep/open_cep.go
@@ -1,1 +1,33 @@
 package openCep
+
+import (
+	"net/http"
+
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+)
+
+var Module = fx.Module(
+	"open_cep",
+	ConfigModule,
+	fx.Provide(NewOpenCep),
+)
+
+type OpenCep struct {
+	client  *http.Client
+	logger  *zap.SugaredLogger
+	BaseUrl *string
+}
+
+func NewOpenCep(c *Config, l *zap.SugaredLogger) *OpenCep {
+	client := &http.Client{}
+	return &OpenCep{
+		client:  client,
+		logger:  l,
+		BaseUrl: c.OpenCepBaseURL,
+	}
+}
+
+func (c *OpenCep) Client() *http.Client {
+	return c.client
+}
