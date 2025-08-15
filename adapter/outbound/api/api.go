f20826bd22945a829bc7757d6a695e8d69e2cf7b
Roberto Filho <zferraz.rf@gmail.com>
2025-08-15 20:57:28 -0300

    feat: include openCep module in API module initialization

--- adapter/outbound/api/api.go
+++ adapter/outbound/api/api.go
@@ -2,10 +2,12 @@ package api
 
 import (
 	"github.com/robertobff/nexpos/adapter/outbound/api/countryStateCity"
+	"github.com/robertobff/nexpos/adapter/outbound/api/openCep"
 	"go.uber.org/fx"
 )
 
 var Module = fx.Module(
 	"api",
 	countryStateCity.Module,
+	openCep.Module,
 )
